fe52010e25bd3bc27f0cb93e6962b6c2a696238c
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-13 04:22:58 +0530

    added session check in gssp in profile page

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,36 @@
-import UserProfile from '../components/profile/user-profile';
+import UserProfile from '../components/profile/user-profile'
+import { getSession } from 'next-auth/client'
+import { useEffect, useState } from 'react'
 
 function ProfilePage() {
-  return <UserProfile />;
+  // const [loading, setLoading] = useState(true)
+  // useEffect(() => {
+  //   getSession().then((session) => {
+  //     if (!session) {
+  //       window.location.href = '/auth'
+  //     } else {
+  //       setLoading(false)
+  //     }
+  //   })
+  // }, [])
+  // if (loading) {
+  //   return <p>Loading...</p>
+  // }
+  return <UserProfile />
 }
 
-export default ProfilePage;
+export async function getServerSideProps(context) {
+  const session = await getSession({ req: context.req })
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/auth',
+        permanent: false,
+      },
+    }
+  }
+  return {
+    props: { session },
+  }
+}
+export default ProfilePage
