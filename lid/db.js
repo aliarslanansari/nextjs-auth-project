dc90315efe62628e0379d608fae52a9ea170a592
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-12 23:59:53 +0530

    created db config and connection

---
+++ lid/db.js
@@ -0,0 +1,14 @@
+import { MongoClient } from 'mongodb'
+
+const uri =
+  'mongodb+srv://ali123:<password>@mongoali.qcgnw.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
+
+const dbConnect = async () => {
+  let client
+  client = await MongoClient.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  return client
+}
+export default dbConnect
