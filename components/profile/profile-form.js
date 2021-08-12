28a8b09677d7782ef0ef6d29ee105d55edeeaf2a
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-13 04:22:02 +0530

    added change password logic on frontend

--- components/profile/profile-form.js
+++ components/profile/profile-form.js
@@ -1,21 +1,35 @@
-import classes from './profile-form.module.css';
+import classes from './profile-form.module.css'
 
 function ProfileForm() {
+  const submitHandler = async (event) => {
+    event.preventDefault()
+    const newPassword = event.target[0].value
+    const oldPassword = event.target[1].value
+    const res = await fetch('/api/user/change-password', {
+      method: 'PATCH',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ newPassword, oldPassword }),
+    })
+    const data = await res.json()
+    console.log(newPassword, oldPassword, data)
+  }
   return (
-    <form className={classes.form}>
+    <form className={classes.form} onSubmit={submitHandler}>
       <div className={classes.control}>
-        <label htmlFor='new-password'>New Password</label>
-        <input type='password' id='new-password' />
+        <label htmlFor="new-password">New Password</label>
+        <input type="password" id="new-password" />
       </div>
       <div className={classes.control}>
-        <label htmlFor='old-password'>Old Password</label>
-        <input type='password' id='old-password' />
+        <label htmlFor="old-password">Old Password</label>
+        <input type="password" id="old-password" />
       </div>
       <div className={classes.action}>
         <button>Change Password</button>
       </div>
     </form>
-  );
+  )
 }
 
-export default ProfileForm;
+export default ProfileForm
