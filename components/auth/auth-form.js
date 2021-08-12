b4b209e1fc5d527ce256c7c0a9bcba83546d843e
Ali Arsalan Ansari <aliarslan1620@gmail.com>
2021-08-13 01:22:20 +0530

    added register login on the frontend

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,29 +1,62 @@
-import { useState } from 'react';
-import classes from './auth-form.module.css';
+import { useState } from 'react'
+import classes from './auth-form.module.css'
 
-function AuthForm() {
-  const [isLogin, setIsLogin] = useState(true);
+const createUser = async (email, password) => {
+  const response = await fetch('/api/auth/signup', {
+    method: 'POST',
+    body: JSON.stringify({ email, password }),
+    headers: {
+      'Content-Type': 'application/json',
+      'Access-Control-Allow-Origin': '*',
+      'Access-Control-Allow-Credentials': true,
+    },
+  })
+  const data = await response.json()
+  if (!response.ok) {
+    throw new Error(data.message || 'Something went wrong!')
+  }
+  return data
+}
 
-  function switchAuthModeHandler() {
-    setIsLogin((prevState) => !prevState);
+const AuthForm = () => {
+  const [isLogin, setIsLogin] = useState(true)
+
+  const switchAuthModeHandler = () => {
+    setIsLogin((prevState) => !prevState)
+  }
+
+  const submitHandler = async (e) => {
+    console.log(e.target[1].value)
+    const email = e.target[0].value
+    const password = e.target[1].value
+    e.preventDefault()
+    if (isLogin) {
+    } else {
+      try {
+        const res = await createUser(email, password)
+        console.log(res)
+      } catch (error) {
+        console.log(error)
+      }
+    }
   }
 
   return (
     <section className={classes.auth}>
       <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
-      <form>
+      <form onSubmit={submitHandler}>
         <div className={classes.control}>
-          <label htmlFor='email'>Your Email</label>
-          <input type='email' id='email' required />
+          <label htmlFor="email">Your Email</label>
+          <input type="email" id="email" required />
         </div>
         <div className={classes.control}>
-          <label htmlFor='password'>Your Password</label>
-          <input type='password' id='password' required />
+          <label htmlFor="password">Your Password</label>
+          <input type="password" id="password" required />
         </div>
         <div className={classes.actions}>
           <button>{isLogin ? 'Login' : 'Create Account'}</button>
           <button
-            type='button'
+            type="button"
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
@@ -32,7 +65,7 @@ function AuthForm() {
         </div>
       </form>
     </section>
-  );
+  )
 }
 
-export default AuthForm;
+export default AuthForm
